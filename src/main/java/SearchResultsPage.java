ce4b2dd59cee892f3241731f4bf3e9fefc3e42e7
Svetleto Norka <b_u@abv.bg>
2024-11-17 19:01:29 +0200

    improvements

--- src/main/java/SearchResultsPage.java
+++ src/main/java/SearchResultsPage.java
@@ -8,38 +8,46 @@ public class SearchResultsPage {
     private WebDriver driver;
 
     private By firstTopCar = By.xpath("(//div[@class='item TOP ']//a[@class='image saveSlink']//img[@class='pic'])[1]");
-    private By phoneNumber = By.xpath("(//div[@class='phone']/text()[normalize-space()])[2]");
+    private By phoneNumber = By.xpath("(//div[@class='phone'])[3]");
 
     public SearchResultsPage(WebDriver driver) {
         this.driver = driver;
     }
 
-  //  public void openFirstCar() {
-//        WebElement firstCar = driver.findElement(firstTopCar);
-//        firstCar.click();
-//    }
-
     public void openFirstCar() {
-        // Изчакване за елемента да бъде видим и кликаем
+        scrollToElement(firstTopCar);
+        waitForElementToBeClickableAndClick(firstTopCar, 10);
+    }
+
+    public String getPhoneNumber() {
+        //scrollToElement(phoneNumber);
+        WebElement phoneNumberElement = driver.findElement(phoneNumber);
+        return phoneNumberElement.getText();
+    }
+
+    public void waitForVisibilityOfElement(By locator) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement firstCar = wait.until(ExpectedConditions.visibilityOfElementLocated(firstTopCar)); // чака, докато елементът стане видим
+        WebElement visibleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
 
-        // Скролиране до елемента
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", firstCar);
-
-        // Изчакване за кликаемост
+    public void waitForElementToBeClickableAndClick(By locator, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
         try {
-            wait.until(ExpectedConditions.elementToBeClickable(firstCar)); // чака, докато елементът стане кликаем
-            firstCar.click();
+            // Wait for the element to be clickable
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            element.click(); // Click the element
         } catch (TimeoutException e) {
-            System.out.println("Елементът не стана кликаем в рамките на зададеното време.");
+            // Log a meaningful message if the element is not clickable
+            System.out.println("The element did not become clickable within " + timeoutInSeconds + " seconds.");
             e.printStackTrace();
         }
     }
 
-    public String getPhoneNumber() {
-        WebElement phoneNumberElement = driver.findElement(phoneNumber);
-        return phoneNumberElement.getText();
+    public void scrollToElement(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement visibleElement = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", visibleElement);
     }
+
 }
